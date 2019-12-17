c23854c27738dd7a1f9efa34a73c8dfb6e83c9d3
sshekhar@uoguelph.ca <sshekhar@SOE-T2319-SA-05.cfs.uoguelph.ca>
2019-12-17 17:21:28 -0500

    First commit. Started by refactoring Sung-Yub's repo

---
+++ models/gbml.py
@@ -0,0 +1,76 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import os
+
+from net.convnet import ConvNet
+from net.resnet import ResNet
+
+# https://github.com/sungyubkim/GBML/blob/master/main.py
+class GBML:
+    '''
+    Gradient-Based Meta-Learning
+    '''
+    def __init__(self, args):
+        self.args = args
+        self.batch_size = self.args.batch_size
+        return None
+
+    def _init_net(self):
+        if self.args.net == 'ConvNet':
+            self.network = ConvNet(self.args)
+        elif self.args.net == 'ResNet':
+            self.network = ResNet(self.args)
+            self.args.hidden_channels = 640
+        self.network.train()
+        self.network.cuda()
+        return None
+
+    def _init_opt(self):
+        if self.args.inner_opt == 'SGD':
+            self.inner_optimizer = torch.optim.SGD(self.network.parameters(), lr=self.args.inner_lr)
+        elif self.args.inner_opt == 'Adam':
+            self.inner_optimizer = torch.optim.Adam(self.network.parameters(), lr=self.args.inner_lr, betas=(0.0, 0.9))
+        else:
+            raise ValueError('Not supported inner optimizer.')
+        if self.args.outer_opt == 'SGD':
+            self.outer_optimizer = torch.optim.SGD(self.network.parameters(), lr=self.args.outer_lr, nesterov=True, momentum=0.9)
+        elif self.args.outer_opt == 'Adam':
+            self.outer_optimizer = torch.optim.Adam(self.network.parameters(), lr=self.args.outer_lr)
+        else:
+            raise ValueError('Not supported outer optimizer.')
+        self.lr_scheduler = torch.optim.lr_scheduler.StepLR(self.outer_optimizer, step_size=10, gamma=0.5)
+        return None
+
+    def unpack_batch(self, batch):
+        train_inputs, train_targets = batch['train']
+        train_inputs = train_inputs.cuda()
+        train_targets = train_targets.cuda()
+
+        test_inputs, test_targets = batch['test']
+        test_inputs = test_inputs.cuda()
+        test_targets = test_targets.cuda()
+        return train_inputs, train_targets, test_inputs, test_targets
+
+    def inner_loop(self):
+        raise NotImplementedError
+
+    def outer_loop(self):
+        raise NotImplementedError
+
+    def lr_sched(self):
+        self.lr_scheduler.step()
+        return None
+
+    def load(self):
+        path = os.path.join(self.args.result_path, self.args.alg, self.args.load_path)
+        self.network.load_state_dict(torch.load(path))
+
+    def load_encoder(self):
+        path = os.path.join(self.args.result_path, self.args.alg, self.args.load_path)
+        self.network.encoder.load_state_dict(torch.load(path))
+
+    def save(self,filename):
+        path = os.path.join(self.args.result_path, self.args.alg, filename)
+        torch.save(self.network.state_dict(), path)
+
---
+++ models/imaml.py
@@ -0,0 +1,105 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import higher
+import copy
+
+from gbml import GBML
+from utils import get_accuracy, apply_grad, mix_grad, grad_to_cos, loss_to_ent
+
+class iMAML(GBML):
+
+    def __init__(self, args):
+        super().__init__(args)
+        self._init_net()
+        self._init_opt()
+        self.lamb = 100.0
+        self.n_cg = args.cg_steps
+        return None
+
+    @torch.enable_grad()
+    def inner_loop(self, fmodel, diffopt, train_input, train_target):
+        
+        train_logit = fmodel(train_input)
+        inner_loss = F.cross_entropy(train_logit, train_target)
+        diffopt.step(inner_loss)
+
+        return None
+
+    @torch.no_grad()
+    def cg(self, in_grad, outer_grad, params):
+        x = outer_grad.clone().detach()
+        r = outer_grad.clone().detach() - self.hv_prod(in_grad, x, params)
+        p = r.clone().detach()
+        for i in range(self.n_cg):
+            Ap = self.hv_prod(in_grad, p, params)
+            alpha = (r @ r)/(p @ Ap)
+            x = x + alpha * p
+            r_new = r - alpha * Ap
+            beta = (r_new @ r_new)/(r @ r)
+            p = r_new + beta * p
+            r = r_new.clone().detach()
+        return self.vec_to_grad(x)
+    
+    def vec_to_grad(self, vec):
+        pointer = 0
+        res = []
+        for param in self.network.parameters():
+            num_param = param.numel()
+            res.append(vec[pointer:pointer+num_param].view_as(param).data)
+            pointer += num_param
+        return res
+
+    @torch.enable_grad()
+    def hv_prod(self, in_grad, x, params):
+        hv = torch.autograd.grad(in_grad, params, retain_graph=True, grad_outputs=x)
+        hv = torch.nn.utils.parameters_to_vector(hv).detach()
+        # precondition with identity matrix
+        return hv/self.lamb + x
+
+    def outer_loop(self, batch, is_train):
+        
+        train_inputs, train_targets, test_inputs, test_targets = self.unpack_batch(batch)
+
+        loss_log = 0
+        acc_log = 0
+        grad_list = []
+        loss_list = []
+
+        for (train_input, train_target, test_input, test_target) in zip(train_inputs, train_targets, test_inputs, test_targets):
+
+            with higher.innerloop_ctx(self.network, self.inner_optimizer, track_higher_grads=False) as (fmodel, diffopt):
+
+                for step in range(self.args.n_inner):
+                    self.inner_loop(fmodel, diffopt, train_input, train_target)
+                
+                train_logit = fmodel(train_input)
+                in_loss = F.cross_entropy(train_logit, train_target)
+
+                test_logit = fmodel(test_input)
+                outer_loss = F.cross_entropy(test_logit, test_target)
+                loss_log += outer_loss.item()/self.batch_size
+
+                with torch.no_grad():
+                    acc_log += get_accuracy(test_logit, test_target).item()/self.batch_size
+            
+                if is_train:
+                    params = list(fmodel.parameters(time=-1))
+                    in_grad = torch.nn.utils.parameters_to_vector(torch.autograd.grad(in_loss, params, create_graph=True))
+                    outer_grad = torch.nn.utils.parameters_to_vector(torch.autograd.grad(outer_loss, params))
+                    implicit_grad = self.cg(in_grad, outer_grad, params)
+                    grad_list.append(implicit_grad)
+                    loss_list.append(outer_loss.item())
+
+        if is_train:
+            self.outer_optimizer.zero_grad()
+            weight = torch.ones(len(grad_list))
+            weight = weight / torch.sum(weight)
+            grad = mix_grad(grad_list, weight)
+            grad_log = apply_grad(self.network, grad)
+            self.outer_optimizer.step()
+            
+            return loss_log, acc_log, grad_log
+        else:
+            return loss_log, acc_log
